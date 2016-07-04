930a456993ce51555cc93edcaaf03881327aef07
Andres <andres@multiplica.com>
2016-07-04 18:26:41 -0500

    Modificar Clase 4 para llamar 2 JSON

--- classes/04/js/services/photos.js
+++ classes/04/js/services/photos.js
@@ -1,5 +1,15 @@
+// app.factory('myphotos', ['$http', function($http) {
+//   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
+//          .success(function(data) {
+//            return data;
+//          })
+//          .error(function(data) {
+//            return data;
+//          });
+// }]);
+
 app.factory('myphotos', ['$http', function($http) {
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
+  return $http.get('js/photos.json')
          .success(function(data) {
            return data;
          })
@@ -8,3 +18,12 @@ app.factory('myphotos', ['$http', function($http) {
          });
 }]);
 
+app.factory('myphotos2', ['$http', function($http) {
+  return $http.get('js/photos2.json')
+         .success(function(data) {
+           return data;
+         })
+         .error(function(data) {
+           return data;
+         });
+}]);
